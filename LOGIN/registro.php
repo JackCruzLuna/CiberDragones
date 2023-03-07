4b8fa35e632912a127aed848caaa9661cff106bd
Laboratorio <10307082@utfv.edu.mx>
2023-03-06 18:08:11 -0600

    registro

--- LOGIN/registro.php
+++ LOGIN/registro.php
@@ -11,17 +11,22 @@ $password = isset($_POST['password']) ? $_POST['password']:'';
 
 
 $con = new SQLite3('data.db');
-$cs = $con -> query("INSERT INTO login (nombre,apaterno,amaterno,email,password) VALUES ('$nombre', '$apaterno','$amaterno', '$email','$password')");
+$cs=$con->query("SELECT * FROM login WHERE email='$email'");
+while ($res= $cs->fetchArray());
+{
+	$correoDos=$res ['email'];
+}
 
-echo 'Datos insertados';
-//echo'
-////<script> window.location="registro.html"
-//</script>
-//';
-echo '
-<html>
-    <meta http-equiv="REFRESH" content="0,url=login.html"> 
-</html>
-';
+if ($correoDos== $email) { 
+	echo '<script> alert("Utilice otro correo"); </script>
+	<script> window.location="login.html" </script>
+	'; 
+	}else{
+		$csDos = $con -> query("INSERT INTO login (nombre,apaterno,amaterno,email,password VALUES ('$nombre', '$apaterno','$amaterno', '$email','$password')");
+		echo '<script> alert("DATOS REGISTRADOS "); </script>
+		<script> window.location="login.html" </script>
+		';
+	}
+
 ?>
  
